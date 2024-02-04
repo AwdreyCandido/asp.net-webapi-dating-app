55238267edba75dd5a3cba4d6b8442228e661a68
AwdreyCandido <awdrey.dev@gmail.com>
2024-02-04 02:15:43 -0300

    initial commit: end of section 2

---
+++ Data/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API;
+
+public class DataContext : DbContext
+{
+    public DataContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    public DbSet<AppUser> Users { get; set; }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+using API;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<DataContext>(opt =>
+{
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+var app = builder.Build();
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
