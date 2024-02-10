7ef4d69097cd47ea4ca4b7ec76b20694fae4fa5c
AwdreyCandido <awdrey.dev@gmail.com>
2024-02-10 00:00:33 -0300

    feat: adding cors to api

--- Program.cs
+++ Program.cs
@@ -8,12 +8,15 @@ builder.Services.AddDbContext<DataContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
 
 var app = builder.Build();
 
-app.UseHttpsRedirection();
+// app.UseHttpsRedirection();
 
-app.UseAuthorization();
+// app.UseAuthorization();
+
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
 
 app.MapControllers();
 
